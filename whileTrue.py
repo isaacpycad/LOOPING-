2713d744ffa4a381b5327fbb592e9c0ade525ccd
Christi_Isaac <68722704+isaacpycad@users.noreply.github.com>
2020-07-28 14:55:32 -0400

    whileTrue.py

---
+++ whileTrue.py
@@ -0,0 +1,10 @@
+#in the place of Boolean True even if you are putting any number other than zero(0) ,doesnt matter negative or positive even a string ex-'hello'
+#prog will be executed because internally 0 is False and all other Data types will consider as non-zero(1 or True)
+#try urself
+
+i=0
+while True:
+    print('isaac')
+    i+=1
+    if i==6:
+        break
